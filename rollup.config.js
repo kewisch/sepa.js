72f514ca4a6fd5b799ad7baebec3bfeef44073c7
Philipp Kewisch <mozilla@kewis.ch>
2026-02-02 17:03:57 +0100

    fix: Allow both named and default exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,25 @@
 import copy from 'rollup-plugin-copy';
 import terser from '@rollup/plugin-terser';
 
+
+const COPY_TYPES = copy({
+  targets: [
+    {src: 'types/sepa.d.ts', dest: 'dist/types'},
+  ]
+});
+
 export default [{
   input: ['lib/module.js'],
   output: [
-    { file: 'dist/sepa.js', format: 'es', exports: 'default', name: 'SEPA' },
-    { file: 'dist/sepa.min.js', format: 'es', exports: 'default', name: 'SEPA', plugins: [terser()] },
-    { file: 'dist/sepa.es5.js', format: 'umd', exports: 'default', name: 'SEPA' },
-    { file: 'dist/sepa.es5.min.js', format: 'umd', exports: 'default', name: 'SEPA', plugins: [terser()] },
+    { file: 'dist/sepa.js', format: 'es' },
+    { file: 'dist/sepa.min.js', format: 'es', plugins: [terser()] },
   ],
-  plugins: [
-    copy({
-      targets: [
-        {src: 'types/sepa.d.ts', dest: 'dist/types'},
-      ]
-    })
-  ]
+  plugins: [COPY_TYPES]
+}, {
+  input: ['lib/module.commonjs.js'],
+  output: [
+    { file: 'dist/sepa.es5.cjs', format: 'umd', exports: 'default', name: 'SEPA' },
+    { file: 'dist/sepa.es5.min.cjs', format: 'umd', exports: 'default', name: 'SEPA', plugins: [terser()] },
+  ],
+  plugins: [COPY_TYPES]
 }];
